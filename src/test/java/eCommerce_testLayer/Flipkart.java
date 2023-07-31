a832efd65e0e9b92660a25cd3e12072455e5ecdf
Tejas Patil <patiltejas4604@gmail.com>
2023-07-31 18:46:20 +0530

    add new project

---
+++ src/test/java/eCommerce_testLayer/Flipkart.java
@@ -0,0 +1,23 @@
+package eCommerce_testLayer;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Flipkart {
+  @Test
+  public void FlipkartTest() throws InterruptedException
+  {
+	  WebDriver d = new ChromeDriver();
+	  d.manage().window().maximize();
+	  d.manage().deleteAllCookies();
+	  d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	  d.get("https://www.flipkart.com/");
+	  
+	  Thread.sleep(5000);
+	  d.quit();
+	  
+  }
+}
